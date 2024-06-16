c669a40b56345e7a32b71c5ae637ae72d6d43894
KrystianGal <krystiann.galka@gmail.com>
2024-06-16 22:56:51 +0200

    delete villa

--- BookingMaster.Web/Controllers/VillaController.cs
+++ BookingMaster.Web/Controllers/VillaController.cs
@@ -54,5 +54,44 @@ namespace BookingMaster.Web.Controllers
             return View(obj);
         }
 
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+           
+            if (ModelState.IsValid && obj.Id>0)
+            {
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+           
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id==obj.Id);
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
