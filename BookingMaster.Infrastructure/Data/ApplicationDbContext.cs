58ca78226974cddfeb7b85a589c79f772838c095
KrystianGal <krystiann.galka@gmail.com>
2024-06-17 19:55:31 +0200

    databse and villanumber view

--- BookingMaster.Infrastructure/Data/ApplicationDbContext.cs
+++ BookingMaster.Infrastructure/Data/ApplicationDbContext.cs
@@ -16,6 +16,7 @@ namespace BookingMaster.Infrastructure.Data
 
 
         public DbSet<Villa> Villas { get; set; }
+        public DbSet<VillaNumber> VillaNumbers { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -33,25 +34,77 @@ namespace BookingMaster.Infrastructure.Data
                 Sqft = 550,
             },
                   new Villa
-                   {
-                  Id = 2,
-                 Name = "Premium Pool Villa",
-              Description = "OPIS.",
-              ImageUrl = "https://placehold.co/600x401",
-             Occupancy = 4,
-             Price = 300,
-              Sqft = 550,
-            },
+                  {
+                      Id = 2,
+                      Name = "Premium Pool Villa",
+                      Description = "OPIS.",
+                      ImageUrl = "https://placehold.co/600x401",
+                      Occupancy = 4,
+                      Price = 300,
+                      Sqft = 550,
+                  },
             new Villa
             {
-              Id = 3,
-             Name = "Luxury Pool Villa",
-              Description = "OPIS.",
-             ImageUrl = "https://placehold.co/600x402",
-             Occupancy = 4,
-             Price = 400,
-              Sqft = 750,
+                Id = 3,
+                Name = "Luxury Pool Villa",
+                Description = "OPIS.",
+                ImageUrl = "https://placehold.co/600x402",
+                Occupancy = 4,
+                Price = 400,
+                Sqft = 750,
             });
+
+
+            modelBuilder.Entity<VillaNumber>().HasData(
+                new VillaNumber
+                {
+                    Villa_Number = 101,
+                    VillaId = 1,
+                },
+                 new VillaNumber
+                 {
+                     Villa_Number = 102,
+                     VillaId = 1,
+                 },
+                  new VillaNumber
+                  {
+                      Villa_Number = 103,
+                      VillaId = 1,
+                  },
+                   new VillaNumber
+                   {
+                       Villa_Number = 104,
+                       VillaId = 1,
+                   },
+                    new VillaNumber
+                    {
+                        Villa_Number = 201,
+                        VillaId = 2,
+                    },
+                     new VillaNumber
+                     {
+                         Villa_Number = 202,
+                         VillaId = 2,
+                     },
+                      new VillaNumber
+                      {
+                          Villa_Number = 203,
+                          VillaId = 2,
+                      },
+                       new VillaNumber
+                       {
+                           Villa_Number = 301,
+                           VillaId = 3,
+                       },
+                        new VillaNumber
+                        {
+                            Villa_Number = 302,
+                            VillaId = 3,
+                        }
+                );
+
+
+            
         }
     }
 }
