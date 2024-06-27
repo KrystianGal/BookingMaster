0eb535bb989e14fccf017882a4f7e2af180711aa
KrystianGal <krystiann.galka@gmail.com>
2024-06-27 21:11:09 +0200

    pierwszy update

---
+++ BookingMaster.Web/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using BookingMaster.Domain.Entities;
+
+namespace BookingMaster.Web.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Villa>? VillaList { get; set; }
+        public DateOnly CheckInDate { get; set; }
+        public DateOnly? CheckOutDate { get; set; }
+        public int Nights { get; set; }
+    }
+}
